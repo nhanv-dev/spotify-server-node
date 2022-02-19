008eba395ba1cce314916bda8f520be32f25ed4c
Trần Thanh Nhân <66198815+nhanv-dev@users.noreply.github.com>
2022-02-19 13:00:34 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const SportifyWebApi = require('spotify-web-api-node');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+const redirectUri = 'https://spotify-client-react.web.app'
+// const redirectUri = 'http://localhost:3000'
 app.use(cors())
 app.use(bodyParser.json());
-
+console.log('Server started')
 app.post('/login', (req, res) => {
     const code = req.body.code
+    console.log(code)
     const sportifyApi = new SportifyWebApi({
-        redirectUri: 'https://spotify-client-react.web.app',
+        redirectUri: redirectUri,
         clientId: 'ff076a0d549947e0a6e78532e2204bea',
         clientSecret: 'c4e0c7f1100f45d3919090e270e4f634'
     })
@@ -32,7 +35,7 @@ app.post('/login', (req, res) => {
 app.post('/refresh', (req, res) => {
     const refreshToken = req.body.refreshToken
     const sportifyApi = new SportifyWebApi({
-        redirectUri: 'https://spotify-client-react.web.app',
+        redirectUri: redirectUri,
         clientId: 'ff076a0d549947e0a6e78532e2204bea',
         clientSecret: 'c4e0c7f1100f45d3919090e270e4f634',
         refreshToken
